e953d1034c9df69b2045eab4d6c552f50824e81a
efrinn <gvaldez@uamv.edu.ni>
2025-04-10 08:38:46 -0600

    Nuevos metodos de mecanografiado agregados

--- pruebas_AnimacionStrings.py
+++ pruebas_AnimacionStrings.py
@@ -12,3 +12,23 @@ def mecanografiar(texto):
 mecanografiar("Hola, soy un texto mecanografiado :D")
 print(" ")
 
+def mecanografiar2(texto):
+    """La funcion recibe el texto en el que deseas hacer el efecto"""
+    for letra in texto:
+        sys.stdout.write(letra) #Escribe la letra en la consola
+        sys.stdout.flush() #Asegura que el texto se imprima inmediatamente
+        time.sleep(0.1) #Espera 0.1 segundos antes de escribir la siguiente letra
+
+mecanografiar2("Saliendo ....")
+
+def mecanografiaBucle(texto):
+    """Hacer un bucle de efecto de maquina de escribir"""
+    while True:
+        for letra in texto:
+            sys.stdout.write(letra) #Escribe la letra en la consola
+            sys.stdout.flush()
+            time.sleep(0.1) #Espera 0.1 segundos antes de escribir la siguiente letra
+        sys.stdout.write("\n") #Regresa el cursor al inicio de la linea
+        sys.stdout.flush()
+        time.sleep(1)
+mecanografiaBucle("......")
